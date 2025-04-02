dfb91c55dd5f7289c50b94ce686f8ab14407416c
Doinel A. <92680721+doinel1a@users.noreply.github.com>
2025-04-02 15:20:59 +0200

    chore: initial set-up from template

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+import './src/env.js';
+
+import type { NextConfig } from 'next';
+
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  webpack: (config: { externals: string[] }) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  }
+} satisfies NextConfig;
+
+export default nextConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import '../styles/globals.css';
+import '../styles/globals.scss';
+
+import React from 'react';
+
+import type { Metadata, Viewport } from 'next';
+
+import config from '_config';
+import { Analytics } from '@vercel/analytics/react';
+
+import Footer from '@/components/footer';
+import GithubCorner from '@/components/github-corner';
+import WithSupportedChains from '@/components/hoc/with-supported-chains';
+import Navbar from '@/components/navbar';
+import RootProvider from '@/components/providers/root';
+import { Toaster } from '@/components/ui/sonner';
+
+export const metadata: Metadata = {
+  title: config.metadata.title,
+  description: config.metadata.description,
+  keywords: config.metadata.keywords,
+  icons: '/favicon.svg',
+  manifest: '/app.webmanifest'
+};
+
+export const viewport: Viewport = {
+  themeColor: '#000'
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body>
+        <RootProvider>
+          <WithSupportedChains>
+            <div className='grid min-h-[100dvh] grid-rows-[auto_1fr_auto]'>
+              <Navbar />
+              {children}
+              <Footer />
+
+              <GithubCorner
+                title='Get started on GitHub'
+                url='https://github.com/doinel1a/next-ts-web3-evm'
+              />
+            </div>
+          </WithSupportedChains>
+
+          <Analytics />
+          <Toaster richColors closeButton />
+        </RootProvider>
+      </body>
+    </html>
+  );
+}
